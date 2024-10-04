03614f51f8d65dc41f74fe332a1e3919da296ca0
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-10-04 15:47:00 -0300

    pagina logradouro implementada

--- AcademiaDoZe/WindowLogradouro.xaml.cs
+++ AcademiaDoZe/WindowLogradouro.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Provider;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,163 @@ namespace AcademiaDoZe;
 /// </summary>
 public partial class WindowLogradouro : Window
 {
-	public WindowLogradouro()
+	private string ConnectionString { get; set; }
+	private string ProviderName { get; set; }
+
+	public WindowLogradouro(string connectionString, string providerName)
 	{
 		InitializeComponent();
+
+		this.Loaded += Page_Loaded;
+
+		this.ConnectionString = connectionString;
+		this.ProviderName = providerName;
+	}
+
+	/// <summary>
+	/// Método void privado, onde quando a pagina carrega ele puxa o metodo "AjustaResources" que vem da classe "ClassFuncoes",
+	/// onde separa entre objeto filho e pai, afim de ajustar o componente do idioma.
+	/// 
+	/// </summary>
+	private void Page_Loaded(object sender, RoutedEventArgs e)
+	{
+		ClassFuncoes.AjustaResources(this);
+	}
+
+	private void txtCep_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCep_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtLogradouro_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtLogradouro_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBairro_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBairro_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void btn_salvar_Click(object sender, RoutedEventArgs e)
+	{
+		Close();
+	}
+
+	private void btn_salvar_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			((Button)sender).RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+		}
+	}
+
+	private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+	{
+		// Mensagem de confirmação
+		MessageBoxResult result = MessageBox.Show(
+			Properties.Idioma.MsgConfirmarFechamento,
+			Properties.Idioma.TituloConfirmarFechamento,
+			MessageBoxButton.YesNo,
+			MessageBoxImage.Question);
+
+		if (result == MessageBoxResult.No)
+		{
+			e.Cancel = true;
+		}
+	}
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			e.Handled = true;
+			TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Next);
+			UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+			if (elementWithFocus != null)
+			{
+				elementWithFocus.MoveFocus(request);
+			}
+		}
+
+		if (e.Key == Key.Escape)
+		{
+			this.Close();
+		}
 	}
 }
