92b8f1f0639cf4955d0135595c50aff0e01f6369
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-10-04 16:34:05 -0300

    Pagina Avaliacao Implementada

--- AcademiaDoZe/WindowAvaliacao.xaml.cs
+++ AcademiaDoZe/WindowAvaliacao.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Provider;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,668 @@ namespace AcademiaDoZe;
 /// </summary>
 public partial class WindowAvaliacao : Window
 {
-	public WindowAvaliacao()
+	private string ConnectionString { get; set; }
+	private string ProviderName { get; set; }
+
+	public WindowAvaliacao(string connectionString, string providerName)
 	{
 		InitializeComponent();
+
+		this.Loaded += Page_Loaded;
+
+		this.ConnectionString = connectionString;
+		this.ProviderName = providerName;
+	}
+
+	private void Page_Loaded(object sender, RoutedEventArgs e)
+	{
+		ClassFuncoes.AjustaResources(this);
+	}
+
+	private void txtCpfAluno_GotFocus_1(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCpfAluno_LostFocus_1(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DataAvaliacao_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DataAvaliacao_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPeso_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPeso_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAltura_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAltura_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtTorax_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtTorax_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCintura_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCintura_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtQuadril_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtQuadril_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtMassaMagra_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtMassaMagra_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtMassaGorda_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtMassaGorda_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAntebracoDir_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAntebracoDir_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAntebracoEsq_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtAntebracoEsq_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBicepsDir_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBicepsDir_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBicepsEsq_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtBicepsEsq_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtImc_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtImc_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPesoIdeal_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPesoIdeal_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCoxaDir_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCoxaDir_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCoxaEsq_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCoxaEsq_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPanturrilhaDir_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPanturrilhaDir_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPanturrilaEsq_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPanturrilaEsq_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPercentualGordura_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtPercentualGordura_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void btn_salvar_Click(object sender, RoutedEventArgs e)
+	{
+		Close();
+	}
+
+	private void btn_salvar_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			((Button)sender).RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+		}
+	}
+
+	private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+	{
+		// Mensagem de confirmação
+		MessageBoxResult result = MessageBox.Show(
+			Properties.Idioma.MsgConfirmarFechamento,
+			Properties.Idioma.TituloConfirmarFechamento,
+			MessageBoxButton.YesNo,
+			MessageBoxImage.Question);
+
+		if (result == MessageBoxResult.No)
+		{
+			e.Cancel = true;
+		}
+	}
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			e.Handled = true;
+			TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Next);
+			UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+			if (elementWithFocus != null)
+			{
+				elementWithFocus.MoveFocus(request);
+			}
+		}
+
+		if (e.Key == Key.Escape)
+		{
+			this.Close();
+		}
 	}
 }
