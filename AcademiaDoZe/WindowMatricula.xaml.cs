41aae24f4e6b756858353cda5784660f9d29b4fa
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-10-04 15:25:27 -0300

    pagina matricula implementada

--- AcademiaDoZe/WindowMatricula.xaml.cs
+++ AcademiaDoZe/WindowMatricula.xaml.cs
@@ -18,8 +18,188 @@ namespace AcademiaDoZe;
 /// </summary>
 public partial class WindowMatricula : Window
 {
-	public WindowMatricula()
+	private string ConnectionString { get; set; }
+	private string ProviderName { get; set; }
+
+	public WindowMatricula(string connectionString, string providerName)
 	{
 		InitializeComponent();
+
+		this.Loaded += Page_Loaded;
+
+		this.ConnectionString = connectionString;
+		this.ProviderName = providerName;
+	}
+
+	private void Page_Loaded(object sender, RoutedEventArgs e)
+	{
+		ClassFuncoes.AjustaResources(this);
+	}
+
+	private void txtCpfAluno_GotFocus_1(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtCpfAluno_LostFocus_1(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DateInicioMatricula_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DateInicioMatricula_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DateFimMatricula_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void DateFimMatricula_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtLaudoMedico_GotFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.LightCyan;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void txtLaudoMedico_LostFocus(object sender, RoutedEventArgs e)
+	{
+		var cor = System.Windows.Media.Brushes.White;
+		if (sender is TextBox)
+		{
+			TextBox textBox = (TextBox)sender;
+			textBox.Background = cor;
+		}
+		else if (sender is PasswordBox)
+		{
+			PasswordBox passwordBox = (PasswordBox)sender;
+			passwordBox.Background = cor;
+		}
+	}
+
+	private void btn_salvar_Click(object sender, RoutedEventArgs e)
+	{
+		Close();
+	}
+
+	private void btn_salvar_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			((Button)sender).RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+		}
+	}
+
+	private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+	{
+		// Mensagem de confirmação
+		MessageBoxResult result = MessageBox.Show(
+			Properties.Idioma.MsgConfirmarFechamento,
+			Properties.Idioma.TituloConfirmarFechamento,
+			MessageBoxButton.YesNo,
+			MessageBoxImage.Question);
+
+		if (result == MessageBoxResult.No)
+		{
+			e.Cancel = true;
+		}
+	}
+
+	private void Window_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			e.Handled = true;
+			TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Next);
+			UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+			if (elementWithFocus != null)
+			{
+				elementWithFocus.MoveFocus(request);
+			}
+		}
+
+		if (e.Key == Key.Escape)
+		{
+			this.Close();
+		}
 	}
 }
